8d766c926d61ac4f7712e05503cccd661a2d34cf
Rubén elGF <rgonzg17@estudiantes.unileon.es>
2019-02-20 17:47:36 +0100

    Añadidas remodelación de la carpeta estructura, más Imprime_Num de la practica 2

---
+++ estructuradecomputadores/practica2/Imprime_Num.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+	{
+	int Num;
+	printf ("Escribe un número: ");
+	scanf ("%d", &Num);
+	printf ("El número introducido es:\n");
+	printf ("Fin del algoritmo\n");
+	return 0;
+	}
